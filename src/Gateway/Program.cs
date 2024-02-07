d76775a92fbd9263b5ae61b12cf32e3eb20133a6
serhiilishchuk <serhii.lishchuk@sigma.software>
2024-02-07 16:59:22 +0200

    Update configuration

--- src/Gateway/Program.cs
+++ src/Gateway/Program.cs
@@ -4,11 +4,13 @@ using HotChocolate.AspNetCore.Voyager;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.HostGraphQLFederation(builder.Configuration);
-
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello from Gateway!");
-app.MapGraphQL();
-//app.UseVoyager("/graphql", "/ghui");
+app
+    .MapGraphQL()
+    .AllowAnonymous();
+
+app.UseVoyager("/graphql", "/ui");
 
 app.Run();
